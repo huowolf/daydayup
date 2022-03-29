3305d710f0bc39554bb85e38e021b44d3b62584c
huowolf <274956285@qq.com>
2022-03-29 22:51:35 +0800

    20220329

--- SpringCloud/集成nacos配置中心/springcloud-nacos/nacos-provider/src/main/java/com/example/controller/HelloController.java
+++ SpringCloud/集成nacos配置中心/springcloud-nacos/nacos-provider/src/main/java/com/example/controller/HelloController.java
@@ -1,13 +1,25 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class HelloController {
+
+
+    @Value("${disableCouponRequest:false}")
+    private Boolean disableCoupon;
 
     @GetMapping("/hello")
     public String hello(){
         return "hello,nacos!";
+    }
+
+    @GetMapping("/getVal")
+    public String getVal(){
+        return "disableCoupon:"+disableCoupon;
     }
 }
