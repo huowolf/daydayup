90000138076b6b4c3a07a3d53494ba430344e162
wxw <274956285@qq.com>
2022-08-08 17:58:41 +0800

    20220808

---
+++ 大数据/spark-example/src/main/scala/MysqlInsertDemo.scala
@@ -0,0 +1,21 @@
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+import java.util.Properties
+
+object MysqlInsertDemo {
+
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("MysqlInsertDemo")
+      .master("local")
+      .getOrCreate()
+
+    val df = spark.read.option("header", "true").csv("input/USER_T.csv")
+    df.show()
+    val url = "jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&useSSL=false&zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Shanghai"
+    val prop = new Properties()
+    prop.put("user", "root")
+    prop.put("password", "root")
+    df.write.mode(SaveMode.Append).jdbc(url, "test_user", prop)
+  }
+}
---
+++ 大数据/spark-example/src/main/scala/MysqlQueryDemo.scala
@@ -0,0 +1,19 @@
+import org.apache.spark.sql.SparkSession
+
+object MysqlQueryDemo {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("MysqlQueryDemo")
+      .master("local")
+      .getOrCreate()
+
+    val jdbcDF = spark.read
+      .format("jdbc")
+      .option("url", "jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&useSSL=false&zeroDateTimeBehavior=convertToNull&serverTimezone=Asia/Shanghai")
+      .option("dbtable", "test_user")
+      .option("user", "root")
+      .option("password", "root")
+      .load()
+    jdbcDF.show()
+  }
+}
